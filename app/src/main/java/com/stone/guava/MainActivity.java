23cfa878b47cde2aff8a2deef75a4097a5686cd1
stone <aa86799@163.com>
2017-01-25 16:32:09 +0800

    first commit

---
+++ app/src/main/java/com/stone/guava/MainActivity.java
@@ -0,0 +1,19 @@
+package com.stone.guava;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * desc   :
+ * author : stone
+ * email  : aa86799@163.com
+ * time   : 25/01/2017 16 31
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
